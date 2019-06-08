cc27680508c3fb6d008f4ab5dca0a320e2ae7f15
lihsu <lihsu3@gmail.com>
2019-06-08 13:32:31 +0800

    Add basic routes, config Sequelize and add user table

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const app = express()
+const db = require('./models')
 const port = 3000
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
-app.listen(port, () => 
+app.listen(port, () => { 
+	db.sequelize.sync()
 	console.log(`Listening ${port}`)
-)
+})
+
+require('./routes')(app)
