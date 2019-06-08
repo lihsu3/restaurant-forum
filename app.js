4a12b199829ea02d36cafa04d5428417e058b819
lihsu <lihsu3@gmail.com>
2019-06-09 03:41:51 +0800

    Add sign-up info verification and flash messages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const db = require('./models')
 const port = 3000
@@ -9,6 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+
+  next()
+})
 
 app.listen(port, () => { 
 	db.sequelize.sync()
