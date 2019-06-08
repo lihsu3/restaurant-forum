48124aacaa456e1c5b581a6c33f83ddff2fd57f1
lihsu <lihsu3@gmail.com>
2019-06-09 05:46:28 +0800

    Add navbar

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 
-
-
 app.use((req, res, next) => {
+	// console.log(req.user)
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-
+  res.locals.user = req.user
   next()
 })
 
