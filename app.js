cf7ad031391ec86e6455de0363199eeb696ec7b0
lihsu <lihsu3@gmail.com>
2019-06-08 16:00:15 +0800

    Add user sign-up page and control logic

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const db = require('./models')
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
