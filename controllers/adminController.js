54368e76b58d0d7c7c5a8edc92644087c05f5a44
lihsu <lihsu3@gmail.com>
2019-06-09 10:59:16 +0800

    Add restaurant detail info page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -28,6 +28,14 @@ const adminController = {
       res.redirect('/admin/restaurants')
     })
   },
+
+  getRestaurant: (req, res) => {
+  return Restaurant.findByPk(req.params.id).then(restaurant => {
+    return res.render('admin/restaurant', {
+      restaurant: restaurant
+    })
+  })
+}
 }
 
 module.exports = adminController
