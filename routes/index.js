be7fc7748710460a79a75780615ba3f92905a9eb
lihsu <lihsu3@gmail.com>
2019-06-13 16:51:19 +0800

    Add create category function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
+const categoryController = require('../controllers/categoryController')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
@@ -45,4 +46,9 @@ module.exports = (app, passport) => {
   app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
 
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+
+  app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+
 }
