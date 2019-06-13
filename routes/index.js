69969a1de3eceb66509f91f87a6870e77082a7f9
lihsu <lihsu3@gmail.com>
2019-06-13 19:26:17 +0800

    Add category deletion function

--- routes/index.js
+++ routes/index.js
@@ -53,4 +53,6 @@ module.exports = (app, passport) => {
 
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 }
