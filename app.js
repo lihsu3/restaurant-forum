607f60d90edb291cf11a7ce088e7c94000a4cc52
lihsu <lihsu3@gmail.com>
2019-07-15 14:10:56 +0800

    build postRestaurant API and refactor

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.engine('handlebars', handlebars({defaultLayout: 'main', helpers: require('./
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 
 app.use(passport.initialize())
--- services/adminService.js
+++ services/adminService.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Category = db.Category
 const Restaurant = db.Restaurant
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = '50e3bcb6876d539'
 
 const adminService = {
 	getRestaurants: (req, res, callback) => {
@@ -31,6 +33,41 @@ const adminService = {
             callback({ status: 'success', message: '' })
           })
       })
+  },
+
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    }
+    const { file } = req // equal to const file = req.file
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.create({
+          name: req.body.name,
+          tel: req.body.tel,
+          address: req.body.address,
+          opening_hours: req.body.opening_hours,
+          description: req.body.description,
+          image: file ? img.data.link : null,
+          CategoryId: req.body.categoryId
+        }).then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        CategoryId: req.body.categoryId
+      })
+        .then((restaurant) => {
+          callback({ status: 'success', message: 'restaurant was successfully created' })
+        })
+    }
   },
 }
 
